a8440fd5e4acd6a6a3c7891aabc6538c92257248
Alpaca10086zyys <2099193046@qq.com>
2024-05-18 12:50:48 +0800

    框架

---
+++ src/views/story2.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="story2">
+      <h1>This is 2</h1>
+      <p>我是张玥2号</p>
+    </div>
+  </template>
---
+++ src/views/story3.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="story3">
+      <h1>This is 3</h1>
+    </div>
+  </template>
