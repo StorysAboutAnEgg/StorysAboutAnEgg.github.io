72198fcf852772c077567878e6a3021eb6d66ef0
StorysAboutAnEgg <167880741+StorysAboutAnEgg@users.noreply.github.com>
2024-06-11 15:56:22 +0800

    Delete dist directory

--- dist/assets/story4-Z-OoWJOa.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-C4ByFR0p.js";const t={},o={class:"story4"},s=Vue.createElementVNode("h1",null,"This is 4",-1),c=[s];function n(_,r){return Vue.openBlock(),Vue.createElementBlock("div",o,c)}const a=e(t,[["render",n]]);export{a as default};
